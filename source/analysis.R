d29fec75fe7d04eafbb84c0ebf4de6c9d02f1d83
MOLLY BANKS <mbanks2@uw.edu>
2022-11-15 21:29:50 -0800

    add summaries

--- source/analysis.R
+++ source/analysis.R
@@ -27,7 +27,7 @@ year_max_ice_jail <- incarceration.data %>%
 county_max_ice_jail <- incarceration.data %>%
   filter(total_jail_from_ice == max(total_jail_from_ice, na.rm = TRUE)) %>%
   pull(county_name)
-  
+
 #----------------------------------------------------------------------------#
 
 ## Section 3  ----
@@ -60,38 +60,37 @@ plot_jail_pop_for_us <- function() {
 
 ## Section 4  ----
 #----------------------------------------------------------------------------#
-# Growth of Prison Population by State
+#  Prison Population Growth by State
 
 # function takes state codes and returns full state name
 get_full_state_name <- function(states) {
   state_abv <- read.csv(
     file = "/Users/mollybanks/Documents/info201/assignments/a4-mollybanks/source/state_names_and_codes.csv",
-    stringsAsFactors = FALSE)
-  
+    stringsAsFactors = FALSE
+  )
+
   state_names <- state_abv %>%
     filter(Code %in% states, na.rm = TRUE) %>%
     pull(State)
-  
+
   return(state_names)
-  
 }
 
 # This function returns data frame with relevant values
-df_state_prison_pop <- function(states) {
-  state_prison_pop_df <- incarceration.data %>%
+df_state_jail_pop <- function(states) {
+  state_jail_pop_df <- incarceration.data %>%
     filter(state %in% states, na.rm = TRUE) %>%
     select(state, year, total_jail_pop)
-  
+
   return(state_prison_pop_df)
 }
 # This function plots jail pop by state
 plot_jail_pop_by_states <- function(states) {
-  
-  state_labels <-  get_full_state_name(states)# prepares label names for states
-  
-  state_prison_pop_df <- df_state_prison_pop(states) # calls relevant data frame
-    
-  state_jail_pop_plot <- ggplot(data = state_prison_pop_df) +
+  state_labels <- get_full_state_name(states) # prepares label names for states
+
+  state_jail_pop_df <- df_state_jail_pop(states) # calls relevant data frame
+
+  state_jail_pop_plot <- ggplot(data = state_jail_pop_df) +
     geom_smooth(
       mapping = aes(
         x = year,
@@ -101,10 +100,9 @@ plot_jail_pop_by_states <- function(states) {
       se = FALSE
     ) +
     labs(
-      title = "Increase of Jail Population by State (1970-2018)",
+      title = "Jail Population by State (1970-2018)",
       x = "Year",
-      y = "Total Jail Population",
-      caption = "US jail population increase displayed at the state-level"
+      y = "Total Jail Population"
     ) + # aesthetic changes
     scale_color_discrete(name = "States", labels = state_labels)
   return(state_jail_pop_plot)
@@ -118,7 +116,7 @@ plot_jail_pop_by_states <- function(states) {
 inequality_df <- function() {
   inequality_df <- incarceration.data %>%
     select(urbanicity, year, total_jail_pretrial_rate)
-  
+
   return(inequality_df)
 }
 
@@ -131,7 +129,11 @@ inequality_plot <- function() {
       title = "Pretrial Jailing Rates by Urbanicity (1970-2018)",
       x = "Year",
       y = "Jail Pretrial Rate",
-      color = "Urbanicity"
+      color = "Urbanicity", 
+      caption = "Counties marked 'urban' are core metropolitian areas with a
+      population greater than one million. Suburban counties surround urban areas. 
+      Small and midsized counties have populations under one million. 
+      Rural counties have populations under 50,000."
     ) + # aesthetic changes
     scale_color_brewer(palette = "BrBG")
   return(ineq_plot)
@@ -144,38 +146,40 @@ inequality_plot <- function() {
 # this function returns relevant data frame for map
 county_ineq_df <- function(states, years) {
 
-    # returns relevant columns and features for dataframe
+  # returns relevant columns and features for dataframe
   jail_county_ineq_df <- incarceration.data %>%
-    filter(year == years, # filter for year imput
-           state == states) %>% # filter for state imput
-    mutate(subregion = tolower(str_replace(county_name," County", ""))) %>%
-    select(total_jail_pretrial_rate, subregion) 
-  
+    filter(
+      year == years, # filter for year imput
+      state == states
+    ) %>% # filter for state imput
+    mutate(subregion = tolower(str_replace(county_name, " County", ""))) %>%
+    select(total_jail_pretrial_rate, subregion)
+
   # loads dataframe from CSV with state names
   state_abv <- read.csv(
     file = "/Users/mollybanks/Documents/info201/assignments/a4-mollybanks/source/state_names_and_codes.csv",
-    stringsAsFactors = FALSE)
-  
+    stringsAsFactors = FALSE
+  )
+
   state_name <- state_abv %>%
     filter(Code == states) %>%
     summarise(state = tolower(State)) %>%
     pull(state)
-  
+
 
   # joins map data with relevant incarceration data frame from above
   county_ineq_df <- map_data("county") %>%
     filter(region == state_name) %>% # filters map data for state imput
     left_join(jail_county_ineq_df, by = "subregion") # joins by county
-    
+
   return(county_ineq_df)
 }
 
 # returns heatmap of states based on their pretrial jailing rates
 plot_county_ineq <- function(states, years) {
-
   county_ineq_df <- county_ineq_df(states, years) # calls relevant data frame
   title_state <- get_full_state_name(states)
-  
+
   blank_theme <- theme_bw() + # creates minimalist map theme
     theme( # creates minimalist theme for map
       axis.line = element_blank(),
@@ -187,7 +191,7 @@ plot_county_ineq <- function(states, years) {
       panel.grid.minor = element_blank(),
       panel.border = element_blank()
     )
-  
+
   ggplot(county_ineq_df) +
     geom_polygon( # mapping states by coordinates, fill with rate
       mapping = aes(
@@ -200,13 +204,13 @@ plot_county_ineq <- function(states, years) {
       size = .1
     ) +
     coord_map() +
-    scale_fill_continuous(low = "#132B43", high = "Red") +  # aesthetic changes
-    labs(title = paste0("Legally Innocent Jailing Rate in", title_state ),
-         subtitle = paste0("Rate of Pretrial Jailing", " in ", years), 
-         fill = "Jail Pretrial Rate") +
+    scale_fill_continuous(low = "#132B43", high = "Red") + # aesthetic changes
+    labs(
+      title = paste("Legally Innocent Jailing Rate in", title_state, "(by County)"),
+      subtitle = paste("Rate of Pretrial Jail Bookings in", years),
+      fill = "Pretrial Jaling Rate"
+    ) +
     blank_theme
-    
-  }
+}
 
 #----------------------------------------------------------------------------#
-
